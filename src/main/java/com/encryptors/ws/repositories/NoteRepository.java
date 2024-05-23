f05bc7da07d8b5f840bcf35575cd840ccb8fb47e
Nazmi <nazmiyazkan@gmail.com>
2024-05-23 22:27:19 +0300

    project structure fixed

--- src/main/java/com/encryptors/ws/repositories/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.encryptors.ws.repositories;
-
-import com.encryptors.ws.entities.NoteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<NoteEntity, Long> {
-}
